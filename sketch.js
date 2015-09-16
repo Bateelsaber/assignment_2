1ecb149fec6668e2f2f3e83acda6f3d81be3e450
Bateelsaber <sabeb234@newschool.edu>
2015-09-17 00:56:18 +0300

    Background

--- sketch.js
+++ sketch.js
@@ -1,18 +1,68 @@
+var ballerina;
+var ballerina1;
+
 function setup() {
 	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(640, 460);
+  ballerina = loadImage("imagecopy.png");
+	ballerina1 = loadImage("baimage.png")
 	noStroke();
-	noLoop();
+		noLoop();
+
 }
 
 function draw() {
-	// clear the background
-	background(50, 50, 50);
+	var wallfloor = random(1);
+	// draw background
+	if (wallfloor < 0.5) {
+		// purple background
+		background(81, 54, 111);
+		fill(41, 17, 68);
+		rect(0, 280, 640, 200);
+		fill(61, 68, 110);
+		triangle(0, 280, 0, 0, 280, 280);
+		fill(55, 38, 97);
+		quad(0, 280, 280, 280, 30, 610, -50, 676);
 
-	// set a fill color
-	fill(255, 255, 255);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	} else {
+		// blue background
+		background(59, 55, 111);
+		fill(11, 13, 85);
+		rect(0, 280, 640, 200);
+		fill(61, 68, 110);
+		triangle(0, 280, 0, 0, 280, 280);
+		fill(38, 38, 94);
+		quad(0, 280, 280, 280, 30, 610, -50, 676);
+
+
+	}
+
+
+	if (wallfloor > 0) {
+		fill(250);
+		var dots;
+		dots = random(1, 3);
+
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+		ellipse(random(0, 640), random(0, 278), dots, dots);
+	}
+
+
 }
